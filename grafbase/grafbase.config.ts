c193677c45dd50fb779ad330bc81dcc93edcfd8c
khaifahmi99 <khaifahmi99@github.com>
2023-12-08 18:11:58 +1100

    migrated grafbase to mongo connectors
    major changes include date string type -> datetime type

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,35 @@
+import { graph, config, connector } from '@grafbase/sdk'
+
+const g = graph.Standalone()
+
+const mongo = connector.MongoDB('MongoDB', {
+  url: g.env('MONGODB_API_URL'),
+  apiKey: g.env('MONGODB_API_KEY'),
+  dataSource: g.env('MONGODB_DATASOURCE'),
+  database: g.env('MONGODB_DATABASE'),
+})
+
+mongo.model('Resident', {
+  firstName: g.string(),
+  lastName: g.string(),
+  email: g.email(),
+  phone: g.string().optional(),
+  dob: g.datetime().optional(), 
+}).collection('Resident');
+
+const billTypeEnum = g.enum('BillType', ['Electricity', 'Gas', 'Water', 'Wifi', 'Grocery', 'Others'])
+
+mongo.model('Bill', {
+  title: g.string(),
+  type: g.enumRef(billTypeEnum),
+  amount: g.float(),
+  date: g.datetime(),
+  paid: g.boolean(),
+  paidDate: g.datetime().optional(),
+  description: g.string().optional(),
+  residents: g.ref('Resident').list(),
+}).collection('Bill');
+
+g.datasource(mongo, {namespace: false})
+
+export default config({ schema: g })
