f0741d4b04ea06c60acc25c4d983724619e77550
khaifahmi99 <khaifahmi99@github.com>
2023-12-13 17:42:53 +1100

    revert datetime and email to be string
    appears to not be a valid option when the check is run

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -12,9 +12,9 @@ const mongo = connector.MongoDB('MongoDB', {
 mongo.model('Resident', {
   firstName: g.string(),
   lastName: g.string(),
-  email: g.email().unique(),
+  email: g.string().unique(), // TODO: Use .email() once it is no longer broken
   phone: g.string().optional(),
-  dob: g.datetime().optional(), 
+  dob: g.string().optional(),  // ISO-String
 }).collection('Resident');
 
 const billTypeEnum = g.enum('BillType', ['Electricity', 'Gas', 'Water', 'Wifi', 'Grocery', 'Others'])
@@ -23,9 +23,9 @@ mongo.model('Bill', {
   title: g.string(),
   type: g.enumRef(billTypeEnum),
   amount: g.float(),
-  date: g.datetime(),
+  date: g.string(), // ISO-String
   paid: g.boolean(),
-  paidDate: g.datetime().optional(),
+  paidDate: g.string().optional(), // ISO-String
   description: g.string().optional(),
   residents: g.ref('Resident').list(),
 }).collection('Bill');
